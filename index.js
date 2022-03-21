37a4a5e6590e66c7d3997f5ccd31b8f8adf43e12
Andrea Becerra <andreadeleon729@gmail.com>
2022-03-21 15:52:27 -0600

    Proyecto Pokemon

---
+++ index.js
@@ -0,0 +1,48 @@
+const API = "https://pokeapi.co/api/v2/pokemon/"
+const cartas = document.querySelector(".contenedor")
+
+const botonBuscar = document.getElementById("button-addon2")
+
+async function buscarPK(){
+    const valorInput = document.getElementById("inputPK").value
+    if(valorInput == ""){
+        cartas.innerHTML = 
+        `
+            <div class="alert alert-warning" role="alert">
+                Ingrese algo
+            </div>
+        `
+    }
+    
+    else{
+        try {
+            let pokemones = await fetch(`${API}${valorInput.toLowerCase()}`)
+            let poke = await pokemones.json()
+            let array = []
+            poke.abilities.forEach((pk) => {
+                array.push(pk.ability.name)
+            })
+            console.log(valorInput)
+            cartas.innerHTML = 
+            `
+                <div style="max-width: 350px; background: transparent; width: 18rem; border-radius: 20px; box-shadow: 2px 2px 5px yellow;"" class="card col-4 text-white">
+                    <img style="max-width: 150px;" src="${poke.sprites.other.home.front_default}" class="card-img-top" alt="imagenes de pokemons">
+                    <div class="card-body">
+                    <h5 class="card-title">Detalles del pokemon</h5>
+                    <h6 class="card-text">Nombre: ${poke.name}</h6>
+                    <h6 class="card-text">Habilidades: ${array.toString().split(",").join(", ")}</h6>
+                    </div>
+                </div>
+            `
+        }
+        catch(error){
+            cartas.innerHTML = 
+            `
+                <div class="alert alert-warning" role="alert">
+                    El pokemon que esta intentando buscar no existe
+                </div>
+            `
+        }
+    }
+    document.getElementById("inputPK").value = ""
+}
